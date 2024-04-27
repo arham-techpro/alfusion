c1415b0a0e7b798e55ea0b985fa862617cbcac20
M. Alauddin Bhuiyan <159101808+arham-techpro@users.noreply.github.com>
2024-04-27 16:20:29 +0600

    Full Design Completed

---
+++ assets/js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $('.banner-slider-container').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        dots:true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    // Categoy Slider
+    $('.category-slider').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        arrows:false,
+        dots:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    // Featured Product Slider
+    $('.featured-product-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        arrows:false,
+        dots:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    
+    // Woman Fashion Slider
+    $('.woman-fashion-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        arrows:false,
+        dots:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    
+    // Woman Fashion Slider
+    $('.kids-item-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        arrows:false,
+        dots:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
